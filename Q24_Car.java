4702ab2c69126cae8a1c0eb75a7c76e205d72dec
Mudar Vohra <96954518+MudarVohra@users.noreply.github.com>
2022-01-02 03:35:39 +0530

    Update Q24_Car.java

--- Q24_Car.java
+++ Q24_Car.java
@@ -1,5 +1,5 @@
 /*A87_Mudar Vohra
-Q24.Create a class Car containing features about a car like-color,model,name,company.
+Create a class Car containing features about a car like-color,model,name,company.
 Create its gettter and setter methods to take values and display them.
 */
 import java.util.*;
